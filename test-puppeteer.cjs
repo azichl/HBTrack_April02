d9f15bc6a9778db8e399d4c4aded243ae197940f
Azich <35012864+azichl@users.noreply.github.com>
2026-07-18 13:33:12 +0300

    chore: remove scripts

--- test-puppeteer.cjs
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] });
-  const page = await browser.newPage();
-  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-  page.on('pageerror', err => console.log('PAGE ERROR:', err.message));
-  await page.goto('http://localhost:3000/tracking', { waitUntil: 'networkidle0', timeout: 10000 }).catch(e => console.log(e.message));
-  await browser.close();
-})();
